3dca1b51fa3919a97bd643f4be86e40df5b6564f
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-04 16:06:28 +1100

    Fixed issue where subclasses caused null ref

--- Runtime/WorldEventExtensions.cs
+++ Runtime/WorldEventExtensions.cs
@@ -6,10 +6,25 @@ namespace Sibz.EntityEvents
 {
     public static class WorldEventExtensions
     {
+        public static T GetFirstSystemWithBaseType<T>(this World world)
+            where T : ComponentSystemBase
+        {
+            foreach (ComponentSystemBase worldSystem in world.Systems)
+            {
+                if (worldSystem.GetType() == typeof(EventComponentSystem) ||
+                    worldSystem.GetType().IsSubclassOf(typeof(EventComponentSystem)))
+                {
+                    return worldSystem as T;
+                }
+            }
+
+            return null;
+        }
+
         public static void EnqueueEvent<T>(this World world, T eventData = default)
             where T : struct, IEventComponentData
         {
-            EventComponentSystem system = world.GetExistingSystem<EventComponentSystem>();
+            EventComponentSystem system = world.GetFirstSystemWithBaseType<EventComponentSystem>();
             if (system is null)
             {
                 throw new NullReferenceException($"{nameof(EventComponentSystem)} is null. Unable to enqueue event");
@@ -22,7 +37,7 @@ namespace Sibz.EntityEvents
         public static EnqueueEventJobPart<T> GetEnqueueEventJobPart<T>(this World world, T eventData = default)
             where T : struct, IEventComponentData
         {
-            EventComponentSystem system = world.GetExistingSystem<EventComponentSystem>();
+            EventComponentSystem system = world.GetFirstSystemWithBaseType<EventComponentSystem>();
             if (system is null)
             {
                 throw new NullReferenceException($"{nameof(EventComponentSystem)} is null. Unable to enqueue event");
@@ -33,7 +48,7 @@ namespace Sibz.EntityEvents
 
         public static void EventSystemAddJobDependency(this World world, JobHandle job)
         {
-            EventComponentSystem system = world.GetExistingSystem<EventComponentSystem>();
+            EventComponentSystem system = world.GetFirstSystemWithBaseType<EventComponentSystem>();
             if (system is null)
             {
                 throw new NullReferenceException($"{nameof(EventComponentSystem)} is null. Unable to enqueue event");
